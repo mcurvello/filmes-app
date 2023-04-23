b1cfbfccea38a879f3fdb7dea946a8ccff202402
Marcio Curvello <marciocurvello@gmail.com>
2023-04-23 13:02:39 -0300

    tela de filmes e loading

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { Component } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import api from "./src/services/api";
 import Filmes from "./src/Filmes";
 
@@ -8,30 +15,44 @@ class App extends Component {
     super(props);
     this.state = {
       filmes: [],
+      loading: true,
     };
   }
 
   async componentDidMount() {
     const response = await api.get("r-api/?api=filmes");
-    this.setState({ filmes: response.data });
+    this.setState({ filmes: response.data, loading: false });
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.filmes}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => <Filmes data={item} />}
-        />
-      </View>
-    );
+    if (this.state.loading) {
+      return (
+        <SafeAreaView style={styles.loading}>
+          <ActivityIndicator color="#09a6ff" size={40} />
+        </SafeAreaView>
+      );
+    } else {
+      return (
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={this.state.filmes}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <Filmes data={item} />}
+          />
+        </SafeAreaView>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  loading: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
--- src/Filmes/index.js
+++ src/Filmes/index.js
@@ -1,15 +1,61 @@
 import React, { Component } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 class Filmes extends Component {
   render() {
-    const { nome } = this.props.data;
+    const { nome, foto } = this.props.data;
     return (
       <View>
-        <Text>{nome}</Text>
+        <View style={styles.card}>
+          <Text style={styles.titulo}>{nome}</Text>
+          <Image source={{ uri: foto }} style={styles.capa} />
+          <View style={styles.botaoArea}>
+            <TouchableOpacity style={styles.botao}>
+              <Text style={styles.botaoTexto}>LEIA MAIS</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "#000",
+    backgroundColor: "#fff",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    margin: 15,
+    shadowRadius: 5,
+    borderRadius: 5,
+    elevation: 3,
+  },
+  titulo: {
+    fontSize: 18,
+    padding: 15,
+  },
+  capa: {
+    height: 250,
+    zIndex: 2,
+  },
+  botaoArea: {
+    alignItems: "flex-end",
+    marginTop: -40,
+    zIndex: 9,
+  },
+  botao: {
+    width: 100,
+    backgroundColor: "#09a6ff",
+    opacity: 1,
+    padding: 8,
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+  },
+  botaoTexto: {
+    textAlign: "center",
+    color: "#fff",
+  },
+});
 
 export default Filmes;
